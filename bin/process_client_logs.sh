e27dd597edfc87e661c07ea759777f50862f5d07
Jackson Tenor <tenor031@umn.edu>
2019-09-10 10:29:35 -0500

    Write process_client_logs.sh

---
+++ bin/process_client_logs.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+for file in $@
+do
+	echo "$(awk 'match($0, /([a-zA-Z0-9 ]+):[a-zA-Z0-9_ \[\]:]+ Failed password for ([a-zA-Z0-9]+) from ([0-9/.]+)/, groups) {print groups[1] " " groups[2] " "  groups[3]}' < $file)" >> failed_login_data.txt
+        echo "$(awk 'match($0, /([a-zA-Z0-9 ]+):[a-zA-Z0-9_ \[\]:]+ Failed password for invalid user ([a-zA-Z0-9]+) from ([0-9/.]+)/, groups) {print groups[1] " " groups[2] " " groups[3]}' < $file)" >> failed_login_data.txt
+	
+echo $out0
+done
