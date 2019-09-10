4b176e98da7168223a50091675a1b803cd24d816
Jackson Tenor <tenor031@umn.edu>
2019-09-10 11:33:53 -0500

    Fix issues with failing tests for process_client_logs.sh

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
-for file in $@
+
+rm -f $1/failed_login_data.txt
+
+for file in $(find $1 -type f)
 do
-	echo "$(awk 'match($0, /([a-zA-Z0-9 ]+):[a-zA-Z0-9_ \[\]:]+ Failed password for ([a-zA-Z0-9/-_]+) from ([0-9/.]+)/, groups) {print groups[1] " " groups[2] " "  groups[3]}' < $file)" >> failed_login_data.txt
-        echo "$(awk 'match($0, /([a-zA-Z0-9 ]+):[a-zA-Z0-9_ \[\]:]+ Failed password for invalid user ([a-zA-Z0-9/-_]+) from ([0-9/.]+)/, groups) {print groups[1] " " groups[2] " " groups[3]}' < $file)" >> failed_login_data.txt
-	
-echo $out0
+	echo "$(awk 'match($0, /([a-zA-Z0-9 ]+):[a-zA-Z0-9_ \[\]:]+ Failed password for ([a-zA-Z0-9/-_]+) from ([0-9/.]+)/, groups) {print groups[1] " " groups[2] " "  groups[3]}' < $file)" >> $1/tmp_failed_login_data.txt
+	echo "$(awk 'match($0, /([a-zA-Z0-9 ]+):[a-zA-Z0-9_ \[\]:]+ Failed password for invalid user ([a-zA-Z0-9/-_]+) from ([0-9/.]+)/, groups) {print groups[1] " " groups[2] " " groups[3]}' < $file)" >> $1/tmp_failed_login_data.txt
 done
+
+awk NF $1/tmp_failed_login_data.txt > $1/failed_login_data.txt
+
+rm -f $1/tmp_failed_login_data.txt
+
