4ca592133846fbdc362bd9a1b3ab966495852061
Jackson Tenor <tenor031@umn.edu>
2019-09-10 10:37:32 -0500

    Retrieve failed logins for users with - or _

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -2,8 +2,8 @@
 
 for file in $@
 do
-	echo "$(awk 'match($0, /([a-zA-Z0-9 ]+):[a-zA-Z0-9_ \[\]:]+ Failed password for ([a-zA-Z0-9]+) from ([0-9/.]+)/, groups) {print groups[1] " " groups[2] " "  groups[3]}' < $file)" >> failed_login_data.txt
-        echo "$(awk 'match($0, /([a-zA-Z0-9 ]+):[a-zA-Z0-9_ \[\]:]+ Failed password for invalid user ([a-zA-Z0-9]+) from ([0-9/.]+)/, groups) {print groups[1] " " groups[2] " " groups[3]}' < $file)" >> failed_login_data.txt
+	echo "$(awk 'match($0, /([a-zA-Z0-9 ]+):[a-zA-Z0-9_ \[\]:]+ Failed password for ([a-zA-Z0-9/-_]+) from ([0-9/.]+)/, groups) {print groups[1] " " groups[2] " "  groups[3]}' < $file)" >> failed_login_data.txt
+        echo "$(awk 'match($0, /([a-zA-Z0-9 ]+):[a-zA-Z0-9_ \[\]:]+ Failed password for invalid user ([a-zA-Z0-9/-_]+) from ([0-9/.]+)/, groups) {print groups[1] " " groups[2] " " groups[3]}' < $file)" >> failed_login_data.txt
 	
 echo $out0
 done
