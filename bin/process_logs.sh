9cdb1c01b922a2e774e6c4878b9781192f1e0d61
Jackson Tenor <tenor031@umn.edu>
2019-09-12 11:21:17 -0500

    Combine steps into process_logs.sh
    Tests still fail, but lab is done.

--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -7,5 +7,12 @@ mkdir tmp
 for file in $@
 do
 	mkdir tmp/$(basename $file .tgz)
+	dir=tmp/$(basename $file .tgz)
 	tar -xzf $file -C tmp/$(basename $file .tgz)
+	echo $dir
+	./bin/process_client_logs.sh $dir
+	./bin/create_username_dist.sh $dir
+	./bin/create_hours_dist.sh $dir
+	./bin/create_country_dist.sh $dir
+	./bin/assemble_report.sh $dir
 done
