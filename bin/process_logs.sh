b5be07c1fa05eddbd5978341867966c83289da78
Jackson Tenor <tenor031@umn.edu>
2019-09-17 11:24:24 -0500

    Create proper temp directory

--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env bash
-scratch=tmp
-
-rm -rf tmp
-mkdir tmp
-
-dir=tmp
+dir=$(mktemp -d -t fi-XXXXXXXXXX)
+echo $dir
 for file in $@
 do
-	mkdir tmp/$(basename $file .tgz)
-	tar -xzf $file -C tmp/$(basename $file .tgz)
+	mkdir $dir/$(basename $file .tgz)
+	tar -xzf $file -C $dir/$(basename $file .tgz)
 done
 ./bin/process_client_logs.sh $dir
 ./bin/create_username_dist.sh $dir
 ./bin/create_hours_dist.sh $dir
 ./bin/create_country_dist.sh $dir
 ./bin/assemble_report.sh $dir
-cp tmp/failed_login_summary.html failed_login_summary.html
+cp $dir/failed_login_summary.html failed_login_summary.html
